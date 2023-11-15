fcba8bdc8c23c565451a3f37995e13f2deca992a
N Sushanth Reddy <134855114+sushanth17297@users.noreply.github.com>
2023-11-15 09:23:32 +0530

    deployment commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/",
+  base: "/Album_list_app",
   server: {
     port: 8001,
   },
